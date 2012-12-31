4b4cb9c662ec1c50e9e9729c0847c59612d6cf25
Rich Barrett-Small <rickbs@gmail.com>
2012-12-31 16:47:49 +0000

    New settings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -147,7 +147,7 @@
  *     'authmap'   => 'shared_',
  *   ),
  * @endcode
- * You can also use a reference to a schema/database as a prefix. This maybe
+ * You can also use a reference to a schema/database as a prefix. This may be
  * useful if your Drupal installation exists in a schema that is not the default
  * or you want to access several databases from the same code base at the same
  * time.
@@ -210,7 +210,17 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'sites/default/files/.ht.sqlite',
+      'driver' => 'sqlite',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -242,7 +252,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'DE0ABPxJh2zSINWxAlB0uYMyrclhtuP8nXfbB4mY6wI';
 
 /**
  * Base URL (optional).
@@ -435,7 +445,7 @@ ini_set('session.cookie_lifetime', 2000000);
 /**
  * String overrides:
  *
- * To override specific strings on your site with or without enabling locale
+ * To override specific strings on your site with or without enabling the Locale
  * module, add an entry to this list. This functionality allows you to change
  * a small number of your site's default English language interface strings.
  *
