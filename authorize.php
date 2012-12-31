aca45bd19f438dd054a8b451871440811a2ae079
Rich Barrett-Small <rickbs@gmail.com>
2012-12-31 17:13:25 +0000

    includes from core

--- authorize.php
+++ authorize.php
@@ -31,17 +31,17 @@ define('DRUPAL_ROOT', getcwd());
  * hook_exit() invokes, css/js preprocessing and translation, and
  * solve some theming issues. This flag is checked on several places
  * in Drupal code (not just authorize.php).
- */
+*/
 define('MAINTENANCE_MODE', 'update');
 
 /**
  * Renders a 403 access denied page for authorize.php.
- */
+*/
 function authorize_access_denied_page() {
-  drupal_add_http_header('Status', '403 Forbidden');
-  watchdog('access denied', 'authorize.php', NULL, WATCHDOG_WARNING);
-  drupal_set_title('Access denied');
-  return t('You are not allowed to access this page.');
+	drupal_add_http_header('Status', '403 Forbidden');
+	watchdog('access denied', 'authorize.php', NULL, WATCHDOG_WARNING);
+	drupal_set_title('Access denied');
+	return t('You are not allowed to access this page.');
 }
 
 /**
@@ -54,7 +54,7 @@ function authorize_access_denied_page() {
  *   TRUE if the current user can run authorize.php, otherwise FALSE.
  */
 function authorize_access_allowed() {
-  return variable_get('allow_authorize_operations', TRUE) && user_access('administer software updates');
+	return variable_get('allow_authorize_operations', TRUE) && user_access('administer software updates');
 }
 
 // *** Real work of the script begins here. ***
@@ -93,81 +93,81 @@ $output = '';
 $show_messages = TRUE;
 
 if (authorize_access_allowed()) {
-  // Load both the Form API and Batch API.
-  require_once DRUPAL_ROOT . '/includes/form.inc';
-  require_once DRUPAL_ROOT . '/includes/batch.inc';
-  // Load the code that drives the authorize process.
-  require_once DRUPAL_ROOT . '/includes/authorize.inc';
+	// Load both the Form API and Batch API.
+	require_once DRUPAL_ROOT . '/includes/form.inc';
+	require_once DRUPAL_ROOT . '/includes/batch.inc';
+	// Load the code that drives the authorize process.
+	require_once DRUPAL_ROOT . '/includes/authorize.inc';
 
-  // For the sake of Batch API and a few other low-level functions, we need to
-  // initialize the URL path into $_GET['q']. However, we do not want to raise
-  // our bootstrap level, nor do we want to call drupal_initialize_path(),
-  // since that is assuming that modules are loaded and invoking hooks.
-  // However, all we really care is if we're in the middle of a batch, in which
-  // case $_GET['q'] will already be set, we just initialize it to an empty
-  // string if it's not already defined.
-  if (!isset($_GET['q'])) {
-    $_GET['q'] = '';
-  }
+	// For the sake of Batch API and a few other low-level functions, we need to
+	// initialize the URL path into $_GET['q']. However, we do not want to raise
+	// our bootstrap level, nor do we want to call drupal_initialize_path(),
+	// since that is assuming that modules are loaded and invoking hooks.
+	// However, all we really care is if we're in the middle of a batch, in which
+	// case $_GET['q'] will already be set, we just initialize it to an empty
+	// string if it's not already defined.
+	if (!isset($_GET['q'])) {
+		$_GET['q'] = '';
+	}
 
-  if (isset($_SESSION['authorize_operation']['page_title'])) {
-    drupal_set_title($_SESSION['authorize_operation']['page_title']);
-  }
-  else {
-    drupal_set_title(t('Authorize file system changes'));
-  }
+	if (isset($_SESSION['authorize_operation']['page_title'])) {
+		drupal_set_title($_SESSION['authorize_operation']['page_title']);
+	}
+	else {
+		drupal_set_title(t('Authorize file system changes'));
+	}
 
-  // See if we've run the operation and need to display a report.
-  if (isset($_SESSION['authorize_results']) && $results = $_SESSION['authorize_results']) {
+	// See if we've run the operation and need to display a report.
+	if (isset($_SESSION['authorize_results']) && $results = $_SESSION['authorize_results']) {
 
-    // Clear the session out.
-    unset($_SESSION['authorize_results']);
-    unset($_SESSION['authorize_operation']);
-    unset($_SESSION['authorize_filetransfer_info']);
+		// Clear the session out.
+		unset($_SESSION['authorize_results']);
+		unset($_SESSION['authorize_operation']);
+		unset($_SESSION['authorize_filetransfer_info']);
 
-    if (!empty($results['page_title'])) {
-      drupal_set_title($results['page_title']);
-    }
-    if (!empty($results['page_message'])) {
-      drupal_set_message($results['page_message']['message'], $results['page_message']['type']);
-    }
+		if (!empty($results['page_title'])) {
+			drupal_set_title($results['page_title']);
+		}
+		if (!empty($results['page_message'])) {
+			drupal_set_message($results['page_message']['message'], $results['page_message']['type']);
+		}
 
-    $output = theme('authorize_report', array('messages' => $results['messages']));
+		$output = theme('authorize_report', array('messages' => $results['messages']));
 
-    $links = array();
-    if (is_array($results['tasks'])) {
-      $links += $results['tasks'];
-    }
-    else {
-      $links = array_merge($links, array(
-        l(t('Administration pages'), 'admin'),
-        l(t('Front page'), '<front>'),
-      ));
-    }
+		$links = array();
+		if (is_array($results['tasks'])) {
+			$links += $results['tasks'];
+		}
+		else {
+			$links = array_merge($links, array(
+					l(t('Administration pages'), 'admin'),
+					l(t('Front page'), '<front>'),
+			));
+		}
 
-    $output .= theme('item_list', array('items' => $links, 'title' => t('Next steps')));
-  }
-  // If a batch is running, let it run.
-  elseif (isset($_GET['batch'])) {
-    $output = _batch_page();
-  }
-  else {
-    if (empty($_SESSION['authorize_operation']) || empty($_SESSION['authorize_filetransfer_info'])) {
-      $output = t('It appears you have reached this page in error.');
-    }
-    elseif (!$batch = batch_get()) {
-      // We have a batch to process, show the filetransfer form.
-      $elements = drupal_get_form('authorize_filetransfer_form');
-      $output = drupal_render($elements);
-    }
-  }
-  // We defer the display of messages until all operations are done.
-  $show_messages = !(($batch = batch_get()) && isset($batch['running']));
+		$output .= theme('item_list', array('items' => $links, 'title' => t('Next steps')));
+	}
+	// If a batch is running, let it run.
+	elseif (isset($_GET['batch'])) {
+		$output = _batch_page();
+	}
+	else {
+		if (empty($_SESSION['authorize_operation']) || empty($_SESSION['authorize_filetransfer_info'])) {
+			$output = t('It appears you have reached this page in error.');
+		}
+		elseif (!$batch = batch_get()) {
+			// We have a batch to process, show the filetransfer form.
+			$elements = drupal_get_form('authorize_filetransfer_form');
+			$output = drupal_render($elements);
+		}
+	}
+	// We defer the display of messages until all operations are done.
+	$show_messages = !(($batch = batch_get()) && isset($batch['running']));
 }
 else {
-  $output = authorize_access_denied_page();
+	$output = authorize_access_denied_page();
 }
 
 if (!empty($output)) {
-  print theme('update_page', array('content' => $output, 'show_messages' => $show_messages));
+	print theme('update_page', array('content' => $output, 'show_messages' => $show_messages));
 }
